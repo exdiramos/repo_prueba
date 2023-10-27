2ffbb2cd0568b1bdf95f5b4c60bf30149a1512fe
exdiramos <132028383+exdiramos@users.noreply.github.com>
2023-10-27 12:32:34 -0600

    primer commit

---
+++ src/main/java/com/mycompany/elazul/Elazul.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.elazul;
+
+/**
+ *
+ * @author exdir
+ */
+public class Elazul {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+}
