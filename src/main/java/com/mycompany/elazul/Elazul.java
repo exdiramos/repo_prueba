59dd2684d496151a447fb5211a045be19f4280cc
exdiramos <132028383+exdiramos@users.noreply.github.com>
2023-10-27 12:34:25 -0600

    segundo commit

--- src/main/java/com/mycompany/elazul/Elazul.java
+++ src/main/java/com/mycompany/elazul/Elazul.java
@@ -13,6 +13,9 @@ public class Elazul {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                
+            }
             
         }
     }
